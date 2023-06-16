c2c5ed1f0ae6dad084eb81f333c58daf64f24902
doanthanhdat2509 <doanthanhdat2509@gmail.com>
2023-06-16 17:11:29 +0700

    overall

---
+++ src/components/Operators/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+import Button from "../Button"
+import store from "../../store"
+
+export const opHandler = (type) => {
+    store.newExpression = `${store.curExpression} ${type} `
+}
+
+export const calculateExpression = () => {
+    /* eslint-disable */
+    // This rule is important in production apps!
+    // Read more: https://eslint.org/docs/rules/no-eval
+    // To simplify the functionality in this course we use eval
+    const calcFunc = eval
+    /* eslint-enable */
+    try {
+        store.newExpression = calcFunc(store.curExpression)
+    } catch (e) {
+        console.error("Error: Incorrect Expression of digits & operators :(")
+    }
+}
+
+export const Operators = () => (
+    <section className="buttons--operators">
+        {["+", "-", "*", "/"].map((op, i) => (
+            <Button key={i} text={op} clickHandler={opHandler}/>)
+        )}
+        <Button text="=" clickHandler={calculateExpression}/>
+    </section>
+)
+
+export default Operators
